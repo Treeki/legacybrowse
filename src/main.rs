61da2ad9759913ebbd8cf84bd528762f6600bece
Ash Wolf <ninji@wuffs.org>
2019-01-23 15:31:54 +0000

    first commit with draft proxy using tokio and sslv2 parser

---
+++ src/main.rs
@@ -0,0 +1,378 @@
+#[macro_use]
+extern crate nom;
+extern crate bytes;
+extern crate tokio;
+extern crate tokio_dns;
+extern crate futures;
+extern crate httparse;
+
+mod records;
+
+use tokio::io;
+use tokio::net::{TcpStream, TcpListener};
+use tokio::prelude::*;
+use futures::{Future, Async, Poll};
+use bytes::{BytesMut, BufMut};
+
+
+enum ConnState {
+    WaitingForRequest,
+    ConnectingToHost(tokio_dns::IoFuture<TcpStream>),
+    ConnectProxy,
+    WriteAndShutdown
+}
+
+struct Socket {
+    stream: Option<TcpStream>,
+    write_buf: BytesMut
+}
+
+enum ReadResult {
+    Received(usize),
+    Closed,
+    Err(io::Error),
+    NotReady
+}
+
+enum WriteResult {
+    Sent(usize),
+    Closed,
+    Err(io::Error),
+    NotReady
+}
+
+impl Socket {
+    fn new(stream: Option<TcpStream>) -> Socket {
+        Socket { stream, write_buf: BytesMut::new() }
+    }
+
+    fn is_open(&self) -> bool {
+        return self.stream.is_some();
+    }
+
+    fn read_into(&mut self, buf: &mut BytesMut) -> ReadResult {
+        match &mut self.stream {
+            None         => return ReadResult::Closed,
+            Some(stream) => {
+                buf.reserve(1024);
+                match stream.read_buf(buf) {
+                    Err(err)               => return ReadResult::Err(err),
+                    Ok(Async::NotReady)    => return ReadResult::NotReady,
+                    Ok(Async::Ready(size)) => {
+                        if size == 0 {
+                            return ReadResult::Closed;
+                        } else {
+                            return ReadResult::Received(size);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn write(&mut self) -> WriteResult {
+        match &mut self.stream {
+            None         => return WriteResult::Closed,
+            Some(stream) => {
+                match stream.poll_write(&self.write_buf) {
+                    Err(err)               => return WriteResult::Err(err),
+                    Ok(Async::NotReady)    => return WriteResult::NotReady,
+                    Ok(Async::Ready(size)) => {
+                        self.write_buf.advance(size);
+                        return WriteResult::Sent(size);
+                    }
+                }
+            }
+        }
+    }
+
+    fn write_and_shutdown(&mut self) -> WriteResult {
+        match &mut self.stream {
+            None         => return WriteResult::Closed,
+            Some(stream) => {
+                if self.write_buf.is_empty() {
+                    // shutdown stage reached
+                    match stream.shutdown() {
+                        Ok(Async::NotReady)  => return WriteResult::NotReady,
+                        Ok(Async::Ready(())) => {
+                            self.stream = None;
+                            return WriteResult::Closed;
+                        },
+                        Err(err)             => {
+                            // we can't shut it down, so best we can do is drop it
+                            self.stream = None;
+                            return WriteResult::Err(err);
+                        }
+                    }
+                } else {
+                    // try some writing
+                    match stream.poll_write(&self.write_buf) {
+                        Ok(Async::NotReady)    => return WriteResult::NotReady,
+                        Ok(Async::Ready(size)) => {
+                            self.write_buf.advance(size);
+                            return WriteResult::Sent(size);
+                        },
+                        Err(_)                 => {
+                            // writing failed, so give up on the buffer and try shutting down
+                            self.write_buf.clear();
+                            return self.write_and_shutdown();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ProxyFuture {
+    state: ConnState,
+    initial_buffer: BytesMut,
+    is_tunnel: bool,
+    client: Socket,
+    server: Socket
+}
+
+impl ProxyFuture {
+    fn new(socket: TcpStream) -> ProxyFuture {
+        ProxyFuture {
+            state: ConnState::WaitingForRequest,
+            initial_buffer: BytesMut::new(),
+            is_tunnel: false,
+            client: Socket::new(Some(socket)),
+            server: Socket::new(None),
+        }
+    }
+
+    fn close_with_400(&mut self) {
+        self.client.write_buf.put("HTTP/1.0 400 Bad Request\r\n\r\n");
+        self.state = ConnState::WriteAndShutdown;
+    }
+
+    fn shutdown(&mut self, description: &str) {
+        println!("closing due to {}", description);
+        self.state = ConnState::WriteAndShutdown;
+    }
+
+    fn shutdown_err(&mut self, description: &str, err: io::Error) {
+        println!("closing due to {}: {}", description, err);
+        self.state = ConnState::WriteAndShutdown;
+    }
+
+    fn try_handle_client_request(&mut self) {
+        let mut headers = [httparse::EMPTY_HEADER; 16];
+        let mut request = httparse::Request::new(&mut headers);
+        let request_buffer = self.initial_buffer.take();
+        let parse_result = request.parse(&request_buffer);
+
+        match parse_result {
+            Ok(status) => {
+                if status.is_complete() {
+                    // we've presumably got everything
+                    if let Some("CONNECT") = request.method {
+                        self.initiate_tunnel(&request);
+                    } else {
+                        self.initiate_http_request(&request);
+                    }
+                } else {
+                    // can't do anything yet -- wait for more data
+                    // we'll need to put back the buffer we took out earlier
+                    self.initial_buffer = request_buffer;
+                    return;
+                }
+            },
+            Err(err) => {
+                println!("request parse error: {}", err);
+                self.close_with_400();
+                return;
+            }
+        }
+    }
+
+    fn initiate_http_request(&mut self, request: &httparse::Request) {
+        if request.method.is_none() || request.path.is_none() {
+            self.close_with_400();
+        } else {
+            self.server.write_buf.reserve(16384);
+
+            let method = request.method.unwrap();
+            let full_path = request.path.unwrap();
+            if !full_path.starts_with("http://") {
+                self.close_with_400();
+                return;
+            }
+
+            let full_path = &full_path[7..];
+            let index = full_path.find('/');
+            if index.is_none() {
+                self.close_with_400();
+                return;
+            }
+            let (host, path) = full_path.split_at(index.unwrap());
+            println!("host:{}, path:{}", host, path);
+
+            self.server.write_buf.put(method);
+            self.server.write_buf.put(" ");
+            self.server.write_buf.put(path);
+            self.server.write_buf.put(" HTTP/1.0\r\n");
+
+            for header in request.headers.iter() {
+                self.server.write_buf.put(header.name);
+                self.server.write_buf.put(": ");
+                self.server.write_buf.put(header.value);
+                self.server.write_buf.put("\r\n");
+            }
+            self.server.write_buf.put("\r\n");
+
+            if !host.contains(':') {
+                let future = tokio_dns::TcpStream::connect((host, 80));
+                self.state = ConnState::ConnectingToHost(future);
+            } else {
+                let future = tokio_dns::TcpStream::connect(host);
+                self.state = ConnState::ConnectingToHost(future);
+            }
+            self.is_tunnel = false;
+        }
+    }
+
+    fn initiate_tunnel(&mut self, request: &httparse::Request) {
+        // gonna proxy it
+        match request.path {
+            None => self.close_with_400(),
+            Some(path) => {
+                let future = tokio_dns::TcpStream::connect(path);
+                self.state = ConnState::ConnectingToHost(future);
+                self.is_tunnel = true;
+            }
+        }
+    }
+}
+
+impl Future for ProxyFuture {
+    type Item = ();
+    type Error = io::Error;
+
+    fn poll(&mut self) -> Poll<(), io::Error> {
+        loop {
+            match &mut self.state {
+                ConnState::WaitingForRequest => {
+                    // check what the first stuff we have is
+                    match self.client.read_into(&mut self.initial_buffer) {
+                        ReadResult::NotReady    => return Ok(Async::NotReady),
+                        ReadResult::Err(err)    => {
+                            println!("read err: {:?}", err);
+                            self.state = ConnState::WriteAndShutdown;
+                        },
+                        ReadResult::Closed      => {
+                            println!("connection closed immediately");
+                            self.state = ConnState::WriteAndShutdown;
+                        },
+                        ReadResult::Received(_) => {
+                            self.try_handle_client_request();
+                        }
+                    }
+                },
+                ConnState::ConnectingToHost(future) => {
+                    match future.poll() {
+                        Ok(Async::NotReady) => return Ok(Async::NotReady),
+                        Ok(Async::Ready(stream)) => {
+                            println!("connected to host");
+                            self.server.stream = Some(stream);
+                            self.state = ConnState::ConnectProxy;
+                            if self.is_tunnel {
+                                self.client.write_buf.put("HTTP/1.0 200 OK\r\n\r\n");
+                            }
+                        },
+                        Err(err) => {
+                            println!("connection failed: {}", err);
+                            // TODO figure out how to propagate this correctly
+                            self.close_with_400();
+                        }
+                    }
+                },
+                ConnState::ConnectProxy => {
+                    let mut not_ready = false;
+                    let mut received_stuff = false; // in case we need to loop over again
+
+                    match self.client.read_into(&mut self.server.write_buf) {
+                        ReadResult::Received(n) => { println!("client:{}", n); received_stuff = true; },
+                        ReadResult::NotReady    => not_ready = true,
+                        ReadResult::Err(err)    => self.shutdown_err("client read error", err),
+                        ReadResult::Closed      => self.shutdown("client closed")
+                    }
+
+                    match self.server.read_into(&mut self.client.write_buf) {
+                        ReadResult::Received(n) => { println!("server:{}", n); received_stuff = true; },
+                        ReadResult::NotReady    => not_ready = true,
+                        ReadResult::Err(err)    => self.shutdown_err("server read error", err),
+                        ReadResult::Closed      => self.shutdown("server closed")
+                    }
+
+                    match self.client.write() {
+                        WriteResult::NotReady => not_ready = true,
+                        WriteResult::Err(err) => self.shutdown_err("client write error", err),
+                        _                     => ()
+                    }
+
+                    match self.server.write() {
+                        WriteResult::NotReady => not_ready = true,
+                        WriteResult::Err(err) => self.shutdown_err("server write error", err),
+                        _                     => ()
+                    }
+
+                    if not_ready && !received_stuff {
+                        return Ok(Async::NotReady);
+                    }
+                },
+                ConnState::WriteAndShutdown => {
+                    let mut not_ready = false;
+                    let mut written_stuff = false;
+
+                    match self.client.write_and_shutdown() {
+                        WriteResult::Sent(_)  => written_stuff = true,
+                        WriteResult::NotReady => not_ready = true,
+                        WriteResult::Err(err) => println!("client shutdown error: {}", err),
+                        _                     => ()
+                    }
+
+                    match self.server.write_and_shutdown() {
+                        WriteResult::Sent(_)  => written_stuff = true,
+                        WriteResult::NotReady => not_ready = true,
+                        WriteResult::Err(err) => println!("server shutdown error: {}", err),
+                        _                     => ()
+                    }
+
+                    if not_ready && !written_stuff {
+                        return Ok(Async::NotReady);
+                    }
+                    if !self.client.is_open() && !self.server.is_open() {
+                        println!("all done");
+                        return Ok(Async::Ready(()));
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+
+
+fn main() {
+    let addr = "127.0.0.1:8889".parse().unwrap();
+    let listener = TcpListener::bind(&addr).unwrap();
+
+    let server = listener.incoming().for_each(|socket| {
+        println!("got socket");
+        let handler = ProxyFuture::new(socket)
+        .map_err(|err| {
+            println!("handler error: {}", err);
+        });
+        tokio::spawn(handler);
+        Ok(())
+    })
+    .map_err(|err| {
+        println!("accept error: {:?}", err);
+    });
+
+    tokio::run(server);
+}
---
+++ src/records.rs
@@ -0,0 +1,320 @@
+use nom::{be_u8,be_u16};
+
+use bytes::BufMut;
+
+#[derive(Debug)]
+pub struct SSLv2PackedRecord<'a> {
+    pub data: &'a [u8],
+    pub padding: u8
+}
+
+impl <'a> SSLv2PackedRecord<'a> {
+    pub fn write(&self, to: &mut BufMut) {
+        if self.padding == 0 {
+            let record_length = (self.data.len() as u16) | 0x8000;
+            to.put_u16_be(record_length);
+            to.put_slice(self.data);
+        } else {
+            let record_length = self.data.len() as u16;
+            to.put_u16_be(record_length);
+            to.put_u8(self.padding);
+            to.put_slice(self.data);
+        }
+    }
+}
+
+named!(pub parse_sslv2_packed_record<SSLv2PackedRecord>,
+    do_parse!(
+        record_length: be_u16 >>
+        result: switch!(value!(record_length >> 15),
+            0 => do_parse!(
+                padding: be_u8 >>
+                data: take!(record_length & 0x3FFF) >>
+                (SSLv2PackedRecord { data, padding })
+            ) |
+            1 => do_parse!(
+                data: take!(record_length & 0x7FFF) >>
+                (SSLv2PackedRecord { data, padding: 0 })
+            )
+        ) >>
+        (result)
+    )
+);
+
+#[derive(Debug)]
+pub enum CipherSpec {
+    RC4128WithMD5,            // 0x010080
+    RC4128Export40WithMD5,    // 0x020080
+    RC2128CBCWithMD5,         // 0x030080
+    RC2128CBCExport40WithMD5, // 0x040080
+    IDEA128CBCWithMD5,        // 0x050080
+    DES64CBCWithMD5,          // 0x060040
+    DES192EDE3CBCWithMD5,     // 0x0700C0
+    Unknown(u8, u8, u8)
+}
+
+impl CipherSpec {
+    fn from_tuple(tup: (u8, u8, u8)) -> CipherSpec {
+        match tup {
+            (0x01, 0x00, 0x80) => CipherSpec::RC4128WithMD5,
+            (0x02, 0x00, 0x80) => CipherSpec::RC4128Export40WithMD5,
+            (0x03, 0x00, 0x80) => CipherSpec::RC2128CBCWithMD5,
+            (0x04, 0x00, 0x80) => CipherSpec::RC2128CBCExport40WithMD5,
+            (0x05, 0x00, 0x80) => CipherSpec::IDEA128CBCWithMD5,
+            (0x06, 0x00, 0x40) => CipherSpec::DES64CBCWithMD5,
+            (0x07, 0x00, 0xC0) => CipherSpec::DES192EDE3CBCWithMD5,
+            (a, b, c)          => CipherSpec::Unknown(a, b, c)
+        }
+    }
+
+    fn to_tuple(&self) -> (u8, u8, u8) {
+        match self {
+            CipherSpec::RC4128WithMD5            => (0x01, 0x00, 0x80),
+            CipherSpec::RC4128Export40WithMD5    => (0x02, 0x00, 0x80),
+            CipherSpec::RC2128CBCWithMD5         => (0x03, 0x00, 0x80),
+            CipherSpec::RC2128CBCExport40WithMD5 => (0x04, 0x00, 0x80),
+            CipherSpec::IDEA128CBCWithMD5        => (0x05, 0x00, 0x80),
+            CipherSpec::DES64CBCWithMD5          => (0x06, 0x00, 0x40),
+            CipherSpec::DES192EDE3CBCWithMD5     => (0x07, 0x00, 0xC0),
+            CipherSpec::Unknown(a, b, c)         => (*a, *b, *c)
+        }
+    }
+
+    fn write(&self, to: &mut BufMut) {
+        let (a, b, c) = self.to_tuple();
+        to.put_u8(a);
+        to.put_u8(b);
+        to.put_u8(c);
+    }
+}
+
+named!(parse_cipher_spec<CipherSpec>,
+    map!(tuple!(be_u8, be_u8, be_u8), CipherSpec::from_tuple)
+);
+
+#[derive(Debug)]
+pub struct ClientHello<'a> {
+    pub version: u16,
+    pub cipher_specs: Vec<CipherSpec>,
+    pub session_id: Option<[u8; 16]>,
+    pub challenge: &'a [u8]
+}
+
+impl <'a> ClientHello<'a> {
+    fn write(&self, to: &mut BufMut) {
+        to.put_u16_be(self.version);
+        to.put_u16_be((self.cipher_specs.len() * 3) as u16);
+        to.put_u16_be(if self.session_id.is_some() { 16 } else { 0 });
+        to.put_u16_be(self.challenge.len() as u16);
+        
+        for spec in self.cipher_specs.iter() {
+            spec.write(to);
+        }
+
+        if let Some(session_id) = self.session_id {
+            to.put_slice(&session_id);
+        }
+
+        to.put_slice(self.challenge);
+    }
+}
+
+named!(parse_client_hello<ClientHello>,
+    do_parse!(
+        version: be_u16 >>
+        cipher_specs_length: be_u16 >>
+        session_id_length: verify!(be_u16, |v:u16| v == 0 || v == 16) >>
+        challenge_length: verify!(be_u16, |v:u16| v >= 16 && v <= 32) >>
+        cipher_specs: length_count!(value!(cipher_specs_length / 3), parse_cipher_spec) >>
+        session_id: cond!(session_id_length == 16, count_fixed!(u8, be_u8, 16)) >>
+        challenge: take!(challenge_length) >>
+        (ClientHello { version, cipher_specs, session_id, challenge })
+    )
+);
+
+#[derive(Debug)]
+pub struct ClientMasterKey<'a> {
+    pub cipher_kind: CipherSpec,
+    pub clear_key: &'a [u8],
+    pub encrypted_key: &'a [u8],
+    pub key_arg: &'a [u8]
+}
+
+impl <'a> ClientMasterKey<'a> {
+    fn write(&self, to: &mut BufMut) {
+        self.cipher_kind.write(to);
+        to.put_u16_be(self.clear_key.len() as u16);
+        to.put_u16_be(self.encrypted_key.len() as u16);
+        to.put_u16_be(self.key_arg.len() as u16);
+        to.put_slice(self.clear_key);
+        to.put_slice(self.encrypted_key);
+        to.put_slice(self.key_arg);
+    }
+}
+
+named!(parse_client_master_key<ClientMasterKey>,
+    do_parse!(
+        cipher_kind: parse_cipher_spec >>
+        clear_key_length: be_u16 >>
+        encrypted_key_length: be_u16 >>
+        key_arg_length: be_u16 >>
+        clear_key: take!(clear_key_length) >>
+        encrypted_key: take!(encrypted_key_length) >>
+        key_arg: take!(key_arg_length) >>
+        (ClientMasterKey { cipher_kind, clear_key, encrypted_key, key_arg })
+    )
+);
+
+
+#[derive(Debug)]
+pub struct ClientCertificate<'a> {
+    pub certificate: &'a [u8],
+    pub response: &'a [u8]
+}
+
+impl <'a> ClientCertificate<'a> {
+    fn write(&self, to: &mut BufMut) {
+        to.put_u8(1); // SSL_X509_CERTIFICATE
+        to.put_u16_be(self.certificate.len() as u16);
+        to.put_u16_be(self.response.len() as u16);
+        to.put_slice(self.certificate);
+        to.put_slice(self.response);
+    }
+}
+
+named!(parse_client_certificate<ClientCertificate>,
+    do_parse!(
+        tag!("\x01") >> // SSL_X509_CERTIFICATE
+        certificate_length: be_u16 >>
+        response_length: be_u16 >>
+        certificate: take!(certificate_length) >>
+        response: take!(response_length) >>
+        (ClientCertificate { certificate, response })
+    )
+);
+
+
+
+#[derive(Debug)]
+pub struct ServerHello<'a> {
+    pub session_id_hit: bool,
+    pub has_certificate: bool, // always 0 or SSL_X509_CERTIFICATE
+    pub version: u16,
+    pub certificate: &'a [u8],
+    pub cipher_specs: Vec<CipherSpec>,
+    pub connection_id: &'a [u8]
+}
+
+impl <'a> ServerHello<'a> {
+    fn write(&self, to: &mut BufMut) {
+        to.put_u8(if self.session_id_hit { 1 } else { 0 });
+        to.put_u8(if self.has_certificate { 1 } else { 0 });
+        to.put_u16_be(self.version);
+        to.put_u16_be(self.certificate.len() as u16);
+        to.put_u16_be((self.cipher_specs.len() * 3) as u16);
+        to.put_u16_be(self.connection_id.len() as u16);
+        
+        to.put_slice(self.certificate);
+        for spec in self.cipher_specs.iter() {
+            spec.write(to);
+        }
+        to.put_slice(self.connection_id);
+    }
+}
+
+named!(parse_server_hello<ServerHello>,
+    do_parse!(
+        session_id_hit: map!(be_u8, |v| v != 0) >>
+        has_certificate: map!(verify!(be_u8, |v| v == 0 || v == 1), |v| v == 1) >>
+        version: be_u16 >>
+        certificate_length: be_u16 >>
+        cipher_specs_length: be_u16 >>
+        connection_id_length: verify!(be_u16, |v:u16| v >= 16 && v <= 32) >>
+        certificate: take!(certificate_length) >>
+        cipher_specs: length_count!(value!(cipher_specs_length / 3), parse_cipher_spec) >>
+        connection_id: take!(connection_id_length) >>
+        (ServerHello { session_id_hit, has_certificate, version, certificate, cipher_specs, connection_id })
+    )
+);
+
+named!(read_to_end, map!(nom::rest, |v| v));
+
+named!(parse_request_certificate,
+    do_parse!(
+        tag!("\x01") >> // SSL_AT_MD5_WITH_RSA_ENCRYPTION
+        rest: read_to_end >>
+        (rest)
+    )
+);
+
+
+#[derive(Debug)]
+pub enum SSLv2Record<'a> {
+    Error(u16),
+    ClientHello(ClientHello<'a>),
+    ClientMasterKey(ClientMasterKey<'a>),
+    ClientFinished(&'a [u8]),
+    ServerHello(ServerHello<'a>),
+    ServerVerify(&'a [u8]),
+    ServerFinished(&'a [u8]),
+    RequestCertificate(&'a [u8]),
+    ClientCertificate(ClientCertificate<'a>),
+}
+
+impl <'a> SSLv2Record<'a> {
+    pub fn write(&self, to: &mut BufMut) {
+        match self {
+            SSLv2Record::Error(err) => {
+                to.put_u8(0);
+                to.put_u16_be(*err);
+            },
+            SSLv2Record::ClientHello(v) => {
+                to.put_u8(1);
+                v.write(to);
+            },
+            SSLv2Record::ClientMasterKey(v) => {
+                to.put_u8(2);
+                v.write(to);
+            },
+            SSLv2Record::ClientFinished(v) => {
+                to.put_u8(3);
+                to.put_slice(v);
+            },
+            SSLv2Record::ServerHello(v) => {
+                to.put_u8(4);
+                v.write(to);
+            },
+            SSLv2Record::ServerVerify(v) => {
+                to.put_u8(5);
+                to.put_slice(v);
+            },
+            SSLv2Record::ServerFinished(v) => {
+                to.put_u8(6);
+                to.put_slice(v);
+            },
+            SSLv2Record::RequestCertificate(v) => {
+                to.put_u8(7);
+                to.put_u8(1); // SSL_AT_MD5_WITH_RSA_ENCRYPTION
+                to.put_slice(v);
+            },
+            SSLv2Record::ClientCertificate(v) => {
+                to.put_u8(8);
+                v.write(to);
+            },
+        }
+    }
+}
+
+named!(pub parse_sslv2_record<SSLv2Record>,
+    switch!(be_u8,
+          0 => map!(be_u16,                    |v| SSLv2Record::Error(v))
+        | 1 => map!(parse_client_hello,        |v| SSLv2Record::ClientHello(v))
+        | 2 => map!(parse_client_master_key,   |v| SSLv2Record::ClientMasterKey(v))
+        | 3 => map!(nom::rest,                 |v| SSLv2Record::ClientFinished(v))
+        | 4 => map!(parse_server_hello,        |v| SSLv2Record::ServerHello(v))
+        | 5 => map!(nom::rest,                 |v| SSLv2Record::ServerVerify(v))
+        | 6 => map!(nom::rest,                 |v| SSLv2Record::ServerFinished(v))
+        | 7 => map!(parse_request_certificate, |v| SSLv2Record::RequestCertificate(v))
+        | 8 => map!(parse_client_certificate,  |v| SSLv2Record::ClientCertificate(v))
+    )
+);
